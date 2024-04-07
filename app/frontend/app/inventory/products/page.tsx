921f96815295a646d84ebce96cf93e04ed9b498c
kaori-igawa <kaori0v0igawa@gmail.com>
2024-04-07 13:54:29 +0000

    frontend:商品ページへのリンク

--- app/frontend/app/inventory/products/page.tsx
+++ app/frontend/app/inventory/products/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import productsData from './sample/dummy_products.json';
+import Link from 'next/link';
 
 type ProductData = {
   id: number;
@@ -29,6 +30,8 @@ export default function Page() {
             <th>商品名</th>
             <th>単価</th>
             <th>説明</th>
+            <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -38,6 +41,7 @@ export default function Page() {
               <td>{item.name}</td>
               <td>{item.price}</td>
               <td>{item.description}</td>
+              <td><Link href={`/inventory/products/${item.id}`}>在庫処理</Link></td>
               <td><button>更新・削除</button></td>
             </tr>
           ))}
