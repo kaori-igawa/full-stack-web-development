bd6f95be4057102425c8774fdd5f0a6e9879b1b8
kaori-igawa <kaori0v0igawa@gmail.com>
2024-04-07 14:36:46 +0000

    frontend: 商品の更新削除

--- app/frontend/app/inventory/products/page.tsx
+++ app/frontend/app/inventory/products/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import productsData from './sample/dummy_products.json';
 import Link from 'next/link';
 
@@ -19,10 +19,49 @@ export default function Page() {
     setData(productsData);
   }, [])
 
+  // 新規登録処理
+  const [shownNewRow, setShownNewRow] = useState(false);
+
+  const handleShowNewRow = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setShownNewRow(true);
+  }
+
+  const handleAddCancel = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setShownNewRow(false);
+  }
+
+  const handleAdd = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    // バックエンドを使用した登録処理を呼ぶ
+    setShownNewRow(false);
+  }
+
+  // 更新・削除処理
+  const [editingRow, setEditingRow] = useState(0);
+
+  const handleEditRow: any = (id: number) => {
+    setShownNewRow(false);
+    setEditingRow(id);
+  }
+
+  const handleEditCancel: any = (id: number) => {
+    setEditingRow(0);
+  }
+
+  const handleEdit: any = (id: number) => {
+    setEditingRow(0);
+  }
+
+  const handleDelete: any = (id: number) => {
+    setEditingRow(0);
+  }
+
   return (
     <>
       <h2>商品一覧</h2>
-      <button>商品を追加する</button>
+      <button onClick={handleShowNewRow}>商品を追加する</button>
       <table>
         <thead>
           <tr>
@@ -35,15 +74,43 @@ export default function Page() {
           </tr>
         </thead>
         <tbody>
+          {shownNewRow ? (
+            <tr>
+              <td></td>
+              <td><input type="text" /></td>
+              <td><input type="number" /></td>
+              <td><input type="text" /></td>
+              <td></td>
+              <td>
+                <button onClick={(event) => handleAddCancel(event)}>キャンセル</button>
+                <button onClick={(event) => handleAdd(event)}>登録する</button>
+              </td>
+            </tr>
+          ): ''}
           {data.map((item: ProductData) => (
-            <tr key={item.id}>
+            editingRow === item.id ? (
+              <tr key={item.id}>
+              <td>{item.id}</td>
+              <td><input type="text" defaultValue={item.name} /></td>
+              <td><input type="number" defaultValue={item.price} /></td>
+              <td><input type="text" defaultValue={item.description} /></td>
+              <td></td>
+              <td>
+                <button onClick={() => handleEditCancel(item.id)}>キャンセル</button>
+                <button onClick={() => handleEdit(item.id)}>更新する</button>
+                <button onClick={() =>handleDelete(item.id)}>削除する</button>
+              </td>
+            </tr>
+            ) : (
+              <tr key={item.id}>
               <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.price}</td>
               <td>{item.description}</td>
               <td><Link href={`/inventory/products/${item.id}`}>在庫処理</Link></td>
-              <td><button>更新・削除</button></td>
+              <td><button onClick={() => handleEditRow(item.id)}>更新・削除</button></td>
             </tr>
+            )
           ))}
         </tbody>
       </table>
