d63c19bed79167c4a637ff67c38e2a1f3400841f
kaori-igawa <kaori0v0igawa@gmail.com>
2024-04-11 01:34:37 +0000

    backend:データ取得

--- app/backend/api/inventory/views.py
+++ app/backend/api/inventory/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+
+class ProductView(APIView):
+  """
+  商品操作に関する関数
+  """
+  def get(self, request, format=None):
+    """
+    商品一覧を取得する
+    """
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data, status.HTTP_200_OK)
+  
+class ProductModelViewSet(ModelViewSet):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
--- app/backend/config/urls.py
+++ app/backend/config/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     # api/hello/というリクエストがきたら api/hello/urls.pyの定義をみる
     path('api/hello/', include('api.hello.urls')),
     # api/hello_db/というリクエストがきたら api/hello_db/urls.pyの定義をみる
-    path('api/hello_db/', include('api.hello_db.urls'))
+    path('api/hello_db/', include('api.hello_db.urls')),
+    path('api/inventory/', include('api.inventory.urls')),
 ]
