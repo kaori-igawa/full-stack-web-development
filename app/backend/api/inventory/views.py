8a3a35f9415d5ca4de0462abac182c92eab921cf
kaori-igawa <kaori0v0igawa@gmail.com>
2024-04-16 02:39:43 +0000

    backend:SalesSyncView追加

--- app/backend/api/inventory/views.py
+++ app/backend/api/inventory/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 
 from api.inventory.exception import BusinessException
+from api.inventory.models import Sales, SalesFile, Status
+from api.inventory.serializers import FileSerializer
+
 from django.db.models import F, Value, Sum
 from django.db.models.functions import Coalesce
 from django.conf import settings
 
-# Create your views here.
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+
 from .models import Product, Purchase, Sales
 from .serializers import InventorySerializer, ProductSerializer, PurchaseSerializer, SaleSerializer
 from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 from api.inventory.authentication import RefreshJWTAuthentication
+import pandas
 
 class ProductView(APIView):
   """
@@ -174,7 +178,28 @@ class SalesAsyncView(APIView):
 
 
 class SalesSyncView(APIView):
-  pass
+  def post(self, request, format=None):
+    serializer = FileSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    filename = serializer.validated_data['file'].name
+
+    with open(filename, 'wb') as f:
+      f.write(serializer.validated_data['file'].read())
+
+      sales_file = SalesFile(file_name=filename, status=Status.SYNC)
+      sales_file.save()
+
+      df = pandas.read_csv(filename)
+      for _, row in df.iterrows():
+        sales = Sales(
+          product_id=row['product'],
+          sales_date=row['date'],
+          quantity=row['quantity'],
+          import_file=sales_file
+        )
+        sales.save()
+
+      return Response(status=201)
 
 class SalesList(APIView):
   pass
