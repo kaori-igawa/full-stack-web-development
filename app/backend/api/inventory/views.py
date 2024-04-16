cd9dac777262dce5952bb2016653df75af6e391b
kaori-igawa <kaori0v0igawa@gmail.com>
2024-04-16 08:16:00 +0000

    backend:SalesSyncViewクラスの修正

--- app/backend/api/inventory/views.py
+++ app/backend/api/inventory/views.py
@@ -179,27 +179,22 @@ class SalesAsyncView(APIView):
 
 class SalesSyncView(APIView):
   def post(self, request, format=None):
+
     serializer = FileSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     filename = serializer.validated_data['file'].name
 
-    with open(filename, 'wb') as f:
-      f.write(serializer.validated_data['file'].read())
+    sales_file = SalesFile(file_name=filename, status=Status.SYNC)
+    sales_file.save()
 
-      sales_file = SalesFile(file_name=filename, status=Status.SYNC)
-      sales_file.save()
+    df = pandas.read_csv(serializer.validated_data['file'])
+    for _, row in df.iterrows():
+      sales = Sales(
+        product_id=row['product'], sales_date=row['date'],
+        quantity=row['quantity'], import_file=sales_file)
+      sales.save()
 
-      df = pandas.read_csv(filename)
-      for _, row in df.iterrows():
-        sales = Sales(
-          product_id=row['product'],
-          sales_date=row['date'],
-          quantity=row['quantity'],
-          import_file=sales_file
-        )
-        sales.save()
-
-      return Response(status=201)
+    return Response(status=201)
 
 class SalesList(APIView):
   pass
