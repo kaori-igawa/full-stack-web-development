b9503c7fec801245a492aeb5fcb960b462119c18
kaori-igawa <kaori0v0igawa@gmail.com>
2024-04-18 03:17:38 +0000

    backend:マイグレーションファイルからfixtureを読み込む

---
+++ app/backend/common/migrate_util.py
@@ -0,0 +1,14 @@
+import os
+
+from django.conf import settings
+from django.core.management import call_command
+
+def common_load_fixture(migration_filename):
+
+  setting_file = settings.SETTINGS_MODULE
+
+  target = os.path.splitext(migration_filename)[0].replace('migrations', 'fixtures')
+  base_yaml_name = target + '/base.yaml'
+  print(base_yaml_name)
+
+  call_command('loaddata', '--settings', setting_file, '--format=yaml', base_yaml_name)
